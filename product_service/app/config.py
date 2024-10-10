641b2851cf05fb6f39a08cfd8e5b4c390f4db460
19104146 <19104146@usc.edu.ph>
2024-10-10 20:04:30 +0800

    feat(product): remove CORS

--- product_service/app/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic_settings import BaseSettings
-from typing_extensions import List
-
-
-class Settings(BaseSettings):
-    CORS_ORIGINS: List[str] = ["http://order-service:3003"]
-
-
-settings = Settings()
